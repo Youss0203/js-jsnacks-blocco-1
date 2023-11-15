a763085632d404ca9af6a01b1d3e8fb1f1ec962d
Youss0203 <youssefhalibi003@gmail.com>
2023-11-15 12:27:48 +0100

    aggiunte modifiche dopo correzione

--- cartella snack-4/js/script.js
+++ cartella snack-4/js/script.js
@@ -1,6 +1,11 @@
 
-const number = []
+const newArray = []
  
-for(let i = 0; i <= 100; i++){
-    console.log(i)
+for(let i = 1; i <= 100; i++){
+    if(i%7==0){
+        newArray.push(i)
+    }else if(i%8==0){
+        newArray.push(i)
+    }
 }
+console.log(newArray)
