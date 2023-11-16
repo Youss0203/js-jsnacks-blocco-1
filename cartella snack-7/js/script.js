bf72e016c2ece552b13a96801e9dbc7f1b6d249e
Youss0203 <youssefhalibi003@gmail.com>
2023-11-16 14:29:05 +0100

    inizio snack 7

---
+++ cartella snack-7/js/script.js
@@ -0,0 +1,17 @@
+/*Crea un array vuoto. Chiedi per 6 volte all'utente di inserire un numero,
+ se è dispari inseriscilo nell'array. */
+
+
+ const newArray = [];
+
+ const userFirstNumber = parseInt(prompt("inserisci un numero"));
+ const userSecondNumber = parseInt(prompt("inserisci un numero"));
+ const userThirdNumber = parseInt(prompt("inserisci un numero"));
+ const userFourthNumber = parseInt(prompt("inserisci un numero"));
+ const userFifthNumber = parseInt(prompt("inserisci un numero"));
+ const userSixthNumber = parseInt(prompt("inserisci un numero"));
+
+
+
+
+console.log(newArray)
